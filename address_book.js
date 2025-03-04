d4e4310b9822d100fad16e374feb19d0490577d1
Niraj Kumar <nirajgupta7481@gmail.com>
2025-03-04 13:05:39 +0530

    [Niraj] Add . added validation for contact details using regex patterns

--- address_book.js
+++ address_book.js
@@ -1,3 +1,21 @@
+// UC1: Ability to create a Contact in Address Book
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return `Name: ${this.firstName} ${this.lastName}, Address: ${this.address}, ${this.city}, ${this.state} - ${this.zip}, Phone: ${this.phoneNumber}, Email: ${this.email}`;
+    }
+}
+
 // UC2: Ability to ensure valid contacts are added
 function validateContact(contact) {
     const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
